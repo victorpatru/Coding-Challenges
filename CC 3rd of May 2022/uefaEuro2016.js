3b6c2e0cd9769fea7cda58ef2e976f938ca4e1bc
Victor Patru <patruvictor.mihai@gmail.com>
2022-05-03 20:56:27 +0300

    Coding Challenges for 3rd of March

--- CC 3rd of May 2022/uefaEuro2016.js
+++ CC 3rd of May 2022/uefaEuro2016.js
@@ -3,6 +3,7 @@
 uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
 uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
 uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
 */
 
 const uefaEuro2016 = (teams, scores) => scores[0] === scores[1] ? `At match ${teams[0]} - ${teams[1]}, teams played draw.` : scores[0] > scores[1] ? `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!` : `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
