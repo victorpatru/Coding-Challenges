30412bab7eabcbf31dfd9b250fb93b79feb1440d
Victor Patru <patruvictor.mihai@gmail.com>
2022-05-09 13:54:39 +0300

    Add 9th of May Coding Challenges

---
+++ CC 9th of May 2022/removeExclamationMarks.js
@@ -0,0 +1,9 @@
+/* 8KYU Remove exclamation marks
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+*/
+
+function removeExclamationMarks(s) {
+    return s.split('').filter(item => item !== '!').join('')
+  }
