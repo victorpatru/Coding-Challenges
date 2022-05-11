3086a0d60c4a77bad7a3482d54b87c94d1a9f8d0
Victor Patru <patruvictor.mihai@gmail.com>
2022-05-11 20:31:04 +0300

    Add 11th of May Coding Challenges

---
+++ CC 11th of May 2022/characterRecognitionSoftware.js
@@ -0,0 +1,32 @@
+/* 8KYU Correct the mistakes of the character recognition software
+
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+
+*/
+
+function correct(string) {
+    // your code here
+    let arrayOfWords = string.split('').map(item => {
+      if (item === '5') {
+        item = 'S'
+      } else if (item === '0') {
+        item = 'O'
+      } else if (item === '1') {
+        item = 'I'
+      }
+      
+      return item
+    })
+    
+    return arrayOfWords.join('')
+    
+}
