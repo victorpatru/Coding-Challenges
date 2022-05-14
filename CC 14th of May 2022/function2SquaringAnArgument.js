5bbcbaca53e909868f330f8aee0056c5e33386f6
Victor Patru <patruvictor.mihai@gmail.com>
2022-05-14 21:34:50 +0300

    Add 14th of May Coding Challenges

---
+++ CC 14th of May 2022/function2SquaringAnArgument.js
@@ -0,0 +1,7 @@
+/* 8KYU Function 2 - squaring an argument
+
+How you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+const square = item => item ** 2
---
+++ CC 14th of May 2022/grasshopperTerminalGameCombatFunction.js
@@ -0,0 +1,7 @@
+/* 8KYU Grasshopper - Terminal game combat function
+
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+*/
+
+const combat = (health, damage) => health < damage ? 0 : health - damage
