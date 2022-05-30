d68189911f262caba1a8522acdf06af3b860c70e
Victor Patru <patruvictor.mihai@gmail.com>
2022-05-30 13:36:22 +0300

    Add 30th of May Coding Challenges

---
+++ CC 30th of May 2022/convertToBinary.js
@@ -0,0 +1,7 @@
+/* 8KYU Convert to Binary
+
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+*/
+
+const toBinary = n => +n.toString(2)
