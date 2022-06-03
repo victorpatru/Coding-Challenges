9b7855a3ccfa846304cba0bfb3c81092725d368f
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-03 12:11:20 +0300

    Add 3th of June Coding Challenges

---
+++ CC 3rd of June 2022/formattingDecimalPlaces.js
@@ -0,0 +1,11 @@
+/* 8KYU Formatting decimal places #0
+
+Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+5.5589 is rounded 5.56   
+3.3424 is rounded 3.34
+
+*/
+
+const twoDecimalPlaces = n => +n.toFixed(2)
