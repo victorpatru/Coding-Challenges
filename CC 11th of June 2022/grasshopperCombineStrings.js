1b4c086a8e72a1d6623aaa2de5ccd40ae20db34d
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-11 14:44:00 +0300

    Add 11th of June Coding Challenges

---
+++ CC 11th of June 2022/grasshopperCombineStrings.js
@@ -0,0 +1,16 @@
+/* 8KYU Grasshopper - Combine strings
+
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+*/
+
+// Create the combineNames function here
+const combineNames = (firstName, lastName) => `${firstName} ${lastName}`
---
+++ CC 11th of June 2022/kataExampleTwist.js
@@ -0,0 +1,10 @@
+/* 8KYU Kata Example Twist
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+*/
+
+// add the value "codewars" to the websites array 1,000 times
+const websites = [...Array(1000)].map(item => item = "codewars")
