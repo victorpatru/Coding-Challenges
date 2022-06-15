21b26e58b6ddf5690a88bb6dfbc6b4c190b99098
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-15 14:10:57 +0300

    Add 15th June Coding Challenges

---
+++ CC 15th of June 2022/getAsciiValueChar.js
@@ -0,0 +1,9 @@
+/* 8KYU get ascii value of character
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+
+*/
+
+const getASCII = c => c.charCodeAt(0)
