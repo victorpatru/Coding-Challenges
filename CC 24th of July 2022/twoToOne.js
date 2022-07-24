c20fec6b8f660eb531beac419c616d65c69ffecb
Victor Patru <patruvictor.mihai@gmail.com>
2022-07-24 15:18:22 +0300

    add: 24th of July Coding Challenges

---
+++ CC 24th of July 2022/twoToOne.js
@@ -0,0 +1,19 @@
+/* 7KYU Two to One
+
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+*/
+
+function longest(s1, s2) {
+    // your code
+    return [...new Set(s1.concat(s2))].sort().join('')
+}
+  
